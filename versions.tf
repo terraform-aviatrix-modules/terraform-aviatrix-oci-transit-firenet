665d77b9325309fb882aaa23b8ef88f664479e09
trvsmtchll <trvsmtchll@yahoo.com>
2021-05-12 19:37:52 -0400

    cleanup

--- versions.tf
+++ versions.tf
@@ -2,7 +2,8 @@ terraform {
   required_providers {
     aviatrix = {
       source  = "AviatrixSystems/aviatrix"
-      version = "2.19.0"
+      version = ">= 2.19.0"
     }
   }
+  required_version = ">= 0.13"
 }
